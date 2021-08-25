77b7639b28b2a802a3caa84ccc54fe851fa79775
Dursun KATAR <45147011+dursunkatar@users.noreply.github.com>
2021-08-25 21:56:48 +0300

    Update linkedin.js

--- linkedin.js
+++ linkedin.js
@@ -1,1 +1,33 @@
+      var keywords = [
+        "developer",
+        "yazılım uzmanı",
+        "başvurusu için",
+        "çalışma arkadaşı",
+        "React Native",
+        "ekibin parçası olmak",
+        "Çalışma Arkadaşları",
+      ];
 
+      function eachKeywords(keyword, span) {
+        if (span.textContent.indexOf(keyword) === -1) return;
+        const parent = span.closest("div.feed-shared-update-v2");
+        comment(parent, function () { post(parent); });
+      }
+
+      function post(parent) {
+        setTimeout(function () {
+          parent
+            .querySelector("button.comments-comment-box__submit-button")
+            .click();
+        }, 1000);
+      }
+      function comment(parent, callBack) {
+        parent.querySelector("span.artdeco-button__text").click();
+        setTimeout(function () {
+          parent.querySelector("div.ql-editor").innerHTML = "<p>++</p>";
+          callBack();
+        }, 2000);
+      }
+      document.querySelectorAll('span[dir="ltr"]').forEach((span) => {
+        keywords.forEach((keyword) => eachKeywords(keyword, span));
+      });
